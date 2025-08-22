b4d2426e34ab3e67a442edd7edcc771271feca38
Rute de Almeida <125400636+httpsrute@users.noreply.github.com>
2025-08-21 21:12:33 -0300

    feat: sum sequence with start and end

---
+++ Part02/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumOfASequenceTheSequel {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int sum = 0;
+
+        System.out.println("First number?");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Last number?");
+        int lastNumber = Integer.valueOf(scanner.nextLine());
+
+        for (int i = firstNumber; i <= lastNumber; i++) {
+            sum = sum + i;
+        }
+        
+        System.out.println(sum);
+    }
+}
