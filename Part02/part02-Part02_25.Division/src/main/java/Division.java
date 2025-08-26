a98105085f2411f8eb4f7711e63dfe34563af0c2
Rute de Almeida <125400636+httpsrute@users.noreply.github.com>
2025-08-25 21:07:35 -0300

    feat: divide two numbers

---
+++ Part02/part02-Part02_25.Division/src/main/java/Division.java
@@ -0,0 +1,10 @@
+public class Division {
+
+    public static void main(String[] args) {
+        division(3, 5);
+    }
+
+    public static void division(int numerator, int dominator) {
+        System.out.println((float) numerator / dominator);
+    }
+}
