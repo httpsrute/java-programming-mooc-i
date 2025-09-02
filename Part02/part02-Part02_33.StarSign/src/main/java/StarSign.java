c03f8a730e2b53c0063fc948150f1103857e40f1
Rute de Almeida <125400636+httpsrute@users.noreply.github.com>
2025-09-01 22:52:34 -0300

    feat: add star printer and shape drawer

---
+++ Part02/part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -0,0 +1,39 @@
+public class StarSign {
+
+    public static void main(String[] args) {           
+        printStars(5);
+        System.out.println("\n---");  // printing --- between the shapes
+        printSquare(4);
+        System.out.println("\n---");
+        printRectangle(17, 3);
+        System.out.println("\n---");
+        printTriangle(4);
+    }
+
+    public static void printStars(int number) {
+        for (int i = number; i > 0; i--) {
+            System.out.print("*");  
+        }
+        System.out.print("\n");
+    }
+
+    public static void printSquare(int size) {
+        for (int i = size; i > 0; i--) {
+            printStars(size);           
+        }
+    }
+
+    public static void printRectangle(int width, int height) {
+        for (int i = height; i > 0; i--){
+            for (int j = width; i > 0; i--) {
+                printStars(width);
+            }
+        }
+    }
+
+    public static void printTriangle(int size) {
+        for (int i = 1; i <= size; i++) {
+            printStars(i);
+        }
+    }
+}
