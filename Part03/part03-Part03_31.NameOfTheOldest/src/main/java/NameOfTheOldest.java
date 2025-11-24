708329264529e5adcbe24fe8c8eac54062f2852c
Rute de Almeida <125400636+httpsrute@users.noreply.github.com>
2025-11-24 20:26:52 -0300

    refactor: simplify age comparison logic

--- Part03/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ Part03/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -1,15 +1,15 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NameOfTheOldest {
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            ArrayList<Integer> ages = new ArrayList<>();
-            
-            ArrayList<String> names = new ArrayList<>();
             
             int age = 0;
+            int oldest = 0;
+            
+            String name = "";
+            String oldestPersonName = "";
             
             while (true) {
                 String input = scanner.nextLine();
@@ -19,26 +19,16 @@ public class NameOfTheOldest {
                 }
                 
                 String[] parts = input.split(",");
-                
-                String name = parts[0];
+                               
+                name = parts[0];
                 age = Integer.valueOf(parts[1]);
                 
-                ages.add(age);
-                names.add(name); 
+                if (age > oldest) {
+                    oldest = age;
+                    oldestPersonName = name;
+                }
             }
-            
-            System.out.println(age);
-            
-            int oldest = Integer.MIN_VALUE;
-            String oldestPerson = "";
-            
-            for (int i = 0; i < ages.size(); i++) {
-                if (ages.get(i) > oldest) {
-                    oldest = ages.get(i);
-                    oldestPerson = names.get(i);
-                }              
-            }            
-            System.out.println("Name of the oldest: " + oldestPerson);
+            System.out.println("Name of the oldest: " + oldestPersonName);
         }
     }
 }
