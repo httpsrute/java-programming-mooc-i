2cb29d6c4d308aa3c35018be35387fbf414187f4
Rute de Almeida <125400636+httpsrute@users.noreply.github.com>
2026-01-12 20:37:59 -0300

    feat: create Room class with constructor

---
+++ Part04/part04-Part04_04.Room/src/main/java/Room.java
@@ -0,0 +1,9 @@
+public class Room {
+    private String code;
+    private int seats;
+    
+    public Room(String classCod, int numberOfSeats) {
+        this.code = classCod;
+        this.seats = numberOfSeats;
+    }
+}
