781637ce354355adc0f056f047af08f7e8fef339
Rute de Almeida <125400636+httpsrute@users.noreply.github.com>
2026-01-14 20:31:51 -0300

    refactor: replace print() with toString() in Agent

---
+++ Part04/part04-Part04_13.Agent/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Test your Agent class here
+        
+        Agent agent = new Agent("James", "Bond");
+        
+        System.out.println(agent);
+    }
+}
